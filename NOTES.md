aa8818d2c11ee2674d1a8be36f386b17c04efc63
Jason Vance <jbvance@gmail.com>
2017-12-16 23:24:41 +0000

    create and retrieve lists without items

---
+++ NOTES.md
@@ -0,0 +1,11 @@
+# Step 1: Integrate Basic Theme to Asset Pipeline
+
+[X] / - And see a stubbed out, non-dynamic page with the theme
+
+#Step 2: Allow people to CRUD lists.
+
+ListsController
+    #index
+    - see all their lists
+- create a list
+- show a list
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,22 @@
+class ListsController < ApplicationController
+    def index
+       @lists = List.all 
+       @list = List.new
+    end
+    
+    def show
+       @list = List.find(params[:id])
+    end
+    
+    def create
+      @list = List.new(list_params)
+      @list.save
+      redirect_to list_url(@list)
+    end
+    
+    private
+        
+        def list_params
+           params.require(:list).permit(:name) 
+        end
+end
